d4241463e00028e7d2bf11481cd53f68942777e1
aaron-poplin <aaron@poplin.co>
2023-09-12 13:02:50 -0500

    trying direct update config

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
-import { Dialog } from '@capacitor/dialog';
+import { SplashScreen } from '@capacitor/splash-screen';
 
-CapacitorUpdater.addListener('updateAvailable', async (res) => {
-  try {
-    await Dialog.alert({
-      title: 'Update Available',
-      message: `Version ${res.bundle.version} is available. The app will update now`,
-    })
-    CapacitorUpdater.set(res.bundle)
-  }
-  catch (error) {
-    console.log(error)
-  }
-});
+CapacitorUpdater.addListener('appReady', () => {
+  // Hide splash
+  SplashScreen.hide()
+})
+
+
+// import { Dialog } from '@capacitor/dialog';
+
+// CapacitorUpdater.addListener('updateAvailable', async (res) => {
+//   try {
+//     await Dialog.alert({
+//       title: 'Update Available',
+//       message: `Version ${res.bundle.version} is available. The app will update now`,
+//     })
+//     CapacitorUpdater.set(res.bundle)
+//   }
+//   catch (error) {
+//     console.log(error)
+//   }
+// });
 
 CapacitorUpdater.notifyAppReady();
 
