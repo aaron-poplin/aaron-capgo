68337bee90e101dd5e9ea9c5da2ae88bc6e63f9d
aaron-poplin <aaron@poplin.co>
2023-09-12 11:10:05 -0500

    capgo setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'aaron.io.ionic.starter',
   appName: 'aaron-capgo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    CapacitorUpdater: {
+      privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEAtyXWBurE+UNxRjFT/BXRyX7UMgV9Tilr6iSsIpcwEJ8NtMia\n1owq8cYlL8+KCnvwLcQPwUx9VE9QcQRGMMfBTLelwVBB9+E9kdgtTVhVII4WN3M9\ntw/xGZqu58CKG/WQHyHdnVB9L27D+fhwTFrYg0JmsDTbZLljtaKhGEWhukz/ky//\nrGR9oehphEMDfxGHg6kvhsl9wKv2LhH34+j7Ild739ec27HKls7cQbCnl9rmMSS8\nztreZ1GsUClo/52pc92ihcyzgpUbhnFSJhJp0mlYAdMGhT4EA8IYByjvtowGbfCk\nQcY1O+87GxG9hMK7RjtF2L6rH19QdfGrIW3UeQIDAQABAoIBADAT6p9eOd8FaVL3\n2Agw4iV2+dU3w7ElBoMspVNfROrYFg0DLEJuAUf+r/xhCue9qUsaUNh5b90WBRPl\n/1cG2SN+gjDBFNAxfKwwh5PqQE6Pfh2ITwcjVDNYe9mynY7BY9gIV9jYLESrWx7f\nrPseTgAHClXvSjiiwh4IVym7/bydz/zjHXP/9UrmZ3IlhJHLq1OAR3gjCVwG9szx\n9ERbe5veAHGh9u7nMHWAwcZncNXEssj3Vm5SoZ8rWLzKIKc05NJ77OWe4+3ZQyZK\njWnWgkq644z3+NpUmco2sYV/+WGKrr5oTlCNFjp4FA5ubwcHbEDCXfcmBKjMu2f8\nBO+LpZECgYEA5xTIDllihnBiB+gGZbHk2KPBZYQOpLc99/MyJB49MZ0Ho/rPQXM4\nKQkG4ALrgspPfUW2fkm24vt7oBZBHcKMDNjuP0ztb4hiZdX8nXrh9AIPz2B8iSbm\nO8OdAKenSqkO2yadZ4jh6YpTxE8t17NWQZA8XueaavvG6no8uTDTgkcCgYEAyuXO\ns2BChtWwP8tNt9KHI23edanEpYFrVQNUeG626wpvvfLsuxrjOxuaC7uvyXrJmeF1\ng8ciMoLL/XaERu7lB7FdCNihOLeETFf7dzy4Ko+Swk7ohvoHP5ufHQZSV5ff2ccw\nw4ZOzAcazS8D5fmQYJcf9Iy613WI+rc05tDekz8CgYA8GoWmZc1gll6JG+p+DPUu\n+/jThLOiWB8AHPtPtaXI/8T6+8NojKVw1u442cmT7TcbqEQ9OMMe6rZO/zrUmMOA\nm/Pr9wBS4DBnuG7KLyoSI28lSz1ZFwsPRGPTxceXfb1ldve99DZ+XgJHryiHUBgB\nWZi6PpdFeFNWrZCZwn9hBQKBgFmaLzRMGFUPXWxuzf2V/ZQJ2yo/NNaaVtoORGmD\nC0i1LS/HbgAbGapqwBbaZgEmNo28YXc2o8WGuatPgY8JxR8mlp1nfZtcgBJu8GBx\n8irpiLhCvLGPDVBr21KoDgkcywCT1NckIvw7Tvy5/JC9K6NcWgJXe9PW1g1Shww/\nxWtlAoGAQHic5ls4nymPYflMCNfWgTIKkU1HHuF9RiRSRfppA/PhehrGAqTVrrml\npQ4hlfvwZZskvjEmwIFs/jXsHztDAfYcGdp5AS9VkSefIap5zpwW6C1+JnHGAvug\nNRuzcRGqwyDym/BotHWPsumSRhRGHg5COcNIQg0z9LMRiR5KZUc=\n-----END RSA PRIVATE KEY-----\n'
+    }
   }
 };
 
