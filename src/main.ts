a6011d9042a7ae6e2464169f9c3a0958e07294b2
aaron-poplin <aaron@poplin.co>
2023-09-16 22:22:06 -0500

    add updateAvail listener

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { Dialog } from '@capacitor/dialog';
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { CapacitorUpdater } from '@capgo/capacitor-updater';
-import { Dialog } from '@capacitor/dialog';
 
 CapacitorUpdater.addListener('updateAvailable', async (res) => {
   try {
-    const { value } = await Dialog.confirm({
-      title: 'Update Available',
-      message: `Version ${res.bundle.version} is available. Would you like to update now?`,
+    await Dialog.alert({
+      title: 'Update Available111',
+      message: `Version ${res.bundle.version} is available. The app will update now`,
     });
-
-    if (value) CapacitorUpdater.set(res.bundle);
+    CapacitorUpdater.set(res.bundle);
   } catch (error) {
     console.log(error);
   }
 });
-
-CapacitorUpdater.notifyAppReady();
 
 if (environment.production) {
   enableProdMode();
